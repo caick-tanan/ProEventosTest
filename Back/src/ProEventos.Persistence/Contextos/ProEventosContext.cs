d05e101f22a1cea6aa0d7b47ea3ed43becbca2d1
Caick Tanan <caick_tanan@hotmail.com>
2022-08-10 12:39:38 -0300

    Seção 6: .NET core - Organizando Camadas

---
+++ Back/src/ProEventos.Persistence/Contextos/ProEventosContext.cs
@@ -0,0 +1,31 @@
+using Microsoft.EntityFrameworkCore;
+using ProEventos.Domain;
+
+namespace ProEventos.Persistence.Contextos
+{
+    public class ProEventosContext : DbContext
+    {
+        public ProEventosContext(DbContextOptions<ProEventosContext> options) 
+            : base(options){}
+        public DbSet<Evento> Eventos { get; set; }
+        public DbSet<Lote> Lotes { get; set; }
+        public DbSet<Palestrante> Palestrantes { get; set; }
+        public DbSet<PalestranteEvento> PalestrantesEventos { get; set; }
+        public DbSet<RedeSocial> RedesSociais { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<PalestranteEvento>()
+            .HasKey(PE => new {PE.EventoId, PE.palestranteId});
+
+            modelBuilder.Entity<Evento>()
+                .HasMany(e => e.RedesSociais)
+                .WithOne(rs => rs.Evento)
+                .OnDelete(DeleteBehavior.Cascade); //serve para deletar em cascata, ou seja irá deletar também as outras FK que estiverem envolvidas
+
+            modelBuilder.Entity<Palestrante>()
+                .HasMany(e => e.RedesSociais)
+                .WithOne(rs => rs.Palestrante)
+                .OnDelete(DeleteBehavior.Cascade);    
+        }
+    }
+}
